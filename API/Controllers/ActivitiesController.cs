c40bb66e2ae584645b3a1ccc6236dcce7c1ee42e
Nguyễn Tuấn Lộc <74365102+NgTuanLoc@users.noreply.github.com>
2024-03-17 14:39:25 +0700

    Setup Walking Skeleton for backend

---
+++ API/Controllers/ActivitiesController.cs
@@ -0,0 +1,21 @@
+﻿using Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace API;
+
+public class ActivitiesController(DataContext context) : BaseApiController
+{
+    private readonly DataContext _context = context;
+    [HttpGet]
+    public async Task<ActionResult<List<Activity>>> GetActivities(CancellationToken cancellationToken)
+    {
+        return await _context.Activities.ToListAsync(cancellationToken);
+    }
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Activity>> GetActivity(Guid id, CancellationToken cancellationToken)
+    {
+        return await _context.Activities.FindAsync([id, cancellationToken], cancellationToken: cancellationToken);
+    }
+}
---
+++ Persistence/DataContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Domain;
+
+namespace Persistence;
+
+public class DataContext(DbContextOptions options) : DbContext(options)
+{
+    public DbSet<Activity> Activities { get; set; }
+}
